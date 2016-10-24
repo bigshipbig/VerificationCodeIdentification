2484e684f0e04fcfae686732b70023cf5bfb6007
bigbigship <417138981@qq.com>
2016-10-24 15:43:02 +0800

    update

--- img/scipts/PictureProcessing.py
+++ img/scipts/PictureProcessing.py
@@ -8,6 +8,7 @@ from PIL import Image,ImageDraw
 G: Integer 图像二值化阀值
 N: Integer 降噪率 0 <N <8
 Z: Integer 降噪次数
+C: Integer 投影法切割阈值
 输出
  0：降噪成功
  1：降噪失败
@@ -17,7 +18,11 @@ class ImageProcessing():
 	#二值数组
 	def __init__(self,Img,tmpt2val={}):
 		self.image = Img
+		print type(Img)
 		self.t2val = tmpt2val
+		self.PictureList = []
+		self.clearimg=""
+		print Img.size
 
 	def twoValue(self,G):
 		for y in xrange(0,self.image.size[1]):
@@ -28,6 +33,7 @@ class ImageProcessing():
 				else:
 					self.t2val[(x,y)] = 0
 
+	#去除图片的噪声
 	def clearNoise(self,N=4,Z=1):
 		for i in xrange(0,Z):
 			self.t2val[(0,0)] = 1
@@ -55,6 +61,7 @@ class ImageProcessing():
 					if nearDots < N:
 						self.t2val[(x,y)] = 1
 
+	#保存二值去噪后的图片					
 	def saveImage(self,filename,size=(100,100)):
 		size = self.image.size
 		tmpimage = Image.new("1",size)
@@ -62,20 +69,56 @@ class ImageProcessing():
 		for x in xrange(0,size[0]):
 			for y in xrange(0,size[1]):
 				draw.point((x,y),self.t2val[(x,y)])
+		self.clearimg = draw
 		tmpimage.save(filename)
-
+	
 	def gett2val(self):
 		return self.t2val
 
 	def getImg(self):
 		return self.image
-
+		
+	#打印二值数组
 	def print_t2val(self):
 		for y in xrange(0,self.image.size[1]):
 			for x in xrange(0,self.image.size[0]):
 				print self.t2val[(x,y)],
 			print
-
+	
+	def drawimg(self,l,r):
+		tmpimage = Image.new("1",(r-l+1,self.image.size[1]))
+		draw = ImageDraw.Draw(tmpimage)
+		if r-l+1 <= 10:
+			return 0
+		print "size = (%d , %d) ,l = %d,r = %d "%(r-l+1,self.image.size[1],l,r)
+		for x in xrange(l,r):
+			for y in xrange(0,self.image.size[1]):
+				draw.point((x-l,y),self.t2val[(x,y)])
+		self.PictureList.append(tmpimage)
+		return 1
+	
+	#投影法切割字符
+	def CharacterSegmentation(self,C=5):
+		pixcount = self.image.size[0]*[0];
+		for x in xrange(0,self.image.size[0]):
+			for y in xrange(0,self.image.size[1]):
+				if self.t2val[(x,y)] == 0:
+					pixcount[x] += 1
+		boundary = [0]
+		for i in range(len(pixcount)):
+			if pixcount[i] < C:
+				boundary.append(i)
+		boundary.append(len(pixcount)-1)
+		start = boundary[0]
+		for i in xrange(1,len(boundary)):
+			if self.drawimg(start,boundary[i]) == 1:
+				start = boundary[i]
+				
+	def showPicList(self):
+		print "len = %d"%(len(self.PictureList))
+		for item in self.PictureList:
+			item.show()
+			
 pp = ImageProcessing(Image.open(sys.argv[1]).convert("L"))
 
 pp.getImg().show()
@@ -88,4 +131,8 @@ pp.saveImage("./tmp.jpg")
 
 Image.open("./tmp.jpg").show()
 
+pp.CharacterSegmentation()
+
 pp.print_t2val() 
+
+pp.showPicList()

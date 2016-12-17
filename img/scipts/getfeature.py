523678bd59af5c7b4d9408a263c2dfbc3e53235b
bigbigship <417138981@qq.com>
2016-12-17 22:43:28 +0800

    get testdata

--- img/scipts/getfeature.py
+++ img/scipts/getfeature.py
@@ -47,5 +47,37 @@ def main():
 	traindata.close()
 	print cnt
 
+def main1():
+	fileList = getimg(config.origin_test_set_path)
+	testdata = open(config.test_set_path,"w")
+	total = len(fileList)
+	id = 1
+	cnt = 0
+	for filename in fileList:
+		print "load testdata %d/%d"%(id,total)
+		id = id + 1
+		pp = ImageProcessing(Image.open(config.origin_test_set_path+"/"+filename).convert("L"))
+		pp.twoValue()
+		pp.clearNoise()
+		pp.CharacterSegmentation()
+		piclist = pp.getPicList()
+		prename = filename.split('.')[0]
+		if len(piclist) < len(prename):
+			print "Picture CharacterSegmentation Error! filename is %s,size = %d"%(filename,len(piclist))
+			cnt = cnt + 1
+			pass
+		else:
+			for index in range(len(piclist)):
+				if index >= 4:
+					continue
+				tmppic = ImageProcessing(piclist[index])
+				s = str(config.label_map[prename[index]])
+				tmppic.twoValue(0)
+				s = s + "," + tmppic.t2_val_to_str()[:-1]
+				testdata.write("%s\n"%s)
+	testdata.close()
+	print cnt
+
 if __name__ == '__main__':
 	main()
+        main1()
